aa5568e1dbfa936615c7a46bc3a2164f5a18709f
Cooker Candle <jamoliddingafurov3223@gmail.com>
2026-01-12 23:28:15 +0500

    data for schedule

---
+++ src/data/mockEvents.js
@@ -0,0 +1,138 @@
+export const mockEvents = [
+    {
+        id: 101,
+        courseId: 1,
+        start: '9:00',
+        duration: 60,
+        type: 'lesson',
+        dayIndex: 0
+    },
+    {
+        id: 102,
+        courseId: 1,
+        start: '9:00',
+        duration: 60,
+        type: 'lesson',
+        dayIndex: 2
+    },
+    {
+        id: 103,
+        courseId: 1,
+        start: '9:00',
+        duration: 60,
+        type: 'lesson',
+        dayIndex: 4
+    },
+    {
+        id: 104,
+        courseId: 1,
+        start: '9:00',
+        duration: 90,
+        type: 'assignment',
+        dayIndex: 5
+    },
+    {
+        id: 105,
+        courseId: 6,
+        start: '14:00',
+        duration: 120,
+        type: 'live-session',
+        dayIndex: 2
+    },
+    {
+        id: 106,
+        courseId: 6,
+        start: '14:00',
+        duration: 120,
+        type: 'live-session',
+        dayIndex: 5
+    },
+    {
+        id: 107,
+        courseId: 11,
+        start: '11:00',
+        duration: 60,
+        type: 'lesson',
+        dayIndex: 0
+    },
+    {
+        id: 108,
+        courseId: 11,
+        start: '14:00',
+        duration: 90,
+        type: 'assignment',
+        dayIndex: 1
+    },
+    {
+        id: 109,
+        courseId: 11,
+        start: '11:00',
+        duration: 60,
+        type: 'lesson',
+        dayIndex: 2
+    },
+    {
+        id: 110,
+        courseId: 11,
+        start: '14:00',
+        duration: 90,
+        type: 'assignment',
+        dayIndex: 3
+    },
+    {
+        id: 111,
+        courseId: 11,
+        start: '15:00',
+        duration: 120,
+        type: 'deadline',
+        dayIndex: 4
+    },
+    {
+        id: 112,
+        courseId: 7,
+        start: '18:00',
+        duration: 75,
+        type: 'live-session',
+        dayIndex: 0
+    },
+    {
+        id: 113,
+        courseId: 7,
+        start: '18:00',
+        duration: 75,
+        type: 'live-session',
+        dayIndex: 1
+    },
+    {
+        id: 114,
+        courseId: 7,
+        start: '18:00',
+        duration: 75,
+        type: 'live-session',
+        dayIndex: 2
+    },
+    {
+        id: 115,
+        courseId: 7,
+        start: '18:00',
+        duration: 75,
+        type: 'live-session',
+        dayIndex: 3
+    },
+    {
+        id: 116,
+        courseId: 7,
+        start: '18:00',
+        duration: 75,
+        type: 'live-session',
+        dayIndex: 4
+    },
+    {
+        id: 117,
+        courseId: 7,
+        start: '18:00',
+        duration: 120,
+        type: 'deadline',
+        dayIndex: 5
+    },
+]
